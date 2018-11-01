6cd18f3da153fedd5cce34ead98410e943d6540f
canzoroglu <canzoroglu@gmail.com>
2018-11-01 15:52:02 +0300

    Add control for test and match methods

--- regexSearch/main.js
+++ regexSearch/main.js
@@ -1,7 +1,18 @@
 let searchString = document.querySelector("#search-string");
 let result = document.querySelector("#result");
 let buttons = document.querySelectorAll("button");
-let inputs = document.querySelectorAll("div.regex-string input");
+let inputs = document.querySelectorAll("div.regex-string input"); //includes regexs
 
 //Add event listener all buttons and bring the value of the input (regex) of the clicked button
-buttons.forEach(button => button.addEventListener("click", evt => console.log(inputs.item(+evt.target.name).value)));
+buttons.forEach(button => button.addEventListener("click", evt => {
+  let buttonNameAttr = +evt.target.name; //coerce value of name attr to number
+  //apply test method
+  if(buttonNameAttr === 0){
+    result.value = new RegExp(inputs[buttonNameAttr].value).test(searchString.value);
+  }
+  //apply match method
+  else if (buttonNameAttr === 1){
+    console.log(new RegExp(inputs[buttonNameAttr].value));
+    result.value = searchString.value.match(new RegExp(inputs[buttonNameAttr].value, "g"));
+  }
+}));
