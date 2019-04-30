d45cfdcccff24a99453384fce19ca4dcc90aa52d
canzoroglu <canzoroglu@gmail.com>
2019-04-30 12:54:02 +0300

    Add logic for creating new questions for flagsNcapitals

--- flagsNcapitals/main.js
+++ flagsNcapitals/main.js
@@ -2,9 +2,69 @@ let startDiv = document.querySelector("#start-div"); //start view
 let flagsDiv = document.querySelector("#flags-div"); //flag game view
 let gameArea = document.querySelector("#game-area");
 let results = document.querySelector("#results");
+let nextButton = document.querySelector("#next");
 let timer = document.querySelector("#time");
 let navItemLink = document.querySelectorAll("li.nav-item a");
 let flagsButton = document.querySelector("a[name='flags']");
+
+const countryAbbr = Object.keys(countries);
+const countryNames = Object.values(countries);
+let flagQuestions = [];
+
+function chooseRandomArrEl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function shuffleArr(arr) {
+    let j, x, i;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+    return arr;
+}
+
+function createQuestion() {
+  while (true) {
+    let question = chooseRandomArrEl(countryAbbr);
+    if (flagQuestions.indexOf(question) === -1) {
+      flagQuestions.push(question);
+      return question;
+    }
+  }
+}
+
+function createOptions(optionNumber = 5) {
+  let flagOptions = [];
+  let question = createQuestion();
+  flagOptions.push(countries[question]);
+  while (flagOptions.length < optionNumber) {
+    let option = chooseRandomArrEl(countryNames);
+    if (flagOptions.indexOf(option) === -1) {
+      flagOptions.push(option);
+    }
+  }
+  return shuffleArr(flagOptions);
+}
+
+// for (let i = 0; i < 5; i++) {
+//   let examples = [];
+//   let src = "";
+//   let options = createOptions();
+//   src = flagQuestions[flagQuestions.length - 1].toLowerCase() + ".svg";
+//   examples.push({flag: src, options});
+//   console.log(examples);
+// }
+
+nextButton.addEventListener("click", () => {
+
+  //Next butonuna tıklanınca seçenekler ve bayraklar değişssin
+  //radio tipindeki inputları seç, bunların value ve labellarını yeni
+  //oluşturulan seçenekler ile değiştir
+
+});
 
 flagsButton.addEventListener("click", () => {
   startDiv.classList.add("d-none");
