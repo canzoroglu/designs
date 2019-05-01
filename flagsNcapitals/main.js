4037c8944e370af62dcc814e9f5bcae017e20582
canzoroglu <canzoroglu@gmail.com>
2019-05-01 10:50:40 +0300

    Update scoreboard with true and false answers for flags

--- flagsNcapitals/main.js
+++ flagsNcapitals/main.js
@@ -1,12 +1,17 @@
 let startDiv = document.querySelector("#start-div"); //start view
 let flagsDiv = document.querySelector("#flags-div"); //flag game view
 let gameArea = document.querySelector("#game-area");
+let correct = document.querySelector("#correct");
+let wrong = document.querySelector("#wrong");
 let radioButtons = document.querySelectorAll("input[name='option']");
 let results = document.querySelector("#results");
 let nextButton = document.querySelector("#next");
 let timer = document.querySelector("#time");
 let navItemLink = document.querySelectorAll("li.nav-item a");
 let flagsButton = document.querySelector("a[name='flags']");
+let checked;
+let rightAns = [], wrongAns = [];
+let trueScore = wrongScore = 0;
 
 const countryAbbr = Object.keys(countries);
 const countryNames = Object.values(countries);
@@ -50,18 +55,43 @@ function createOptions(optionNumber = 5) {
   return shuffleArr(flagOptions);
 }
 
-
-nextButton.addEventListener("click", () => {
+function createGameArea() {
   let options = createOptions();
   let newFlag = "./countries/svg/" + flagQuestions[flagQuestions.length - 1].toLowerCase() + ".svg";
   gameArea.firstElementChild.src = newFlag;
   radioButtons.forEach((radioButton, index) => {
+    radioButton.checked = false;
     radioButton.value = options[index];
     radioButton.nextElementSibling.textContent = options[index];
   });
+}
+
+radioButtons.forEach(button => {
+  button.addEventListener("click", () => {
+      checked = button.value;
+  });
+});
+
+nextButton.addEventListener("click", () => {
+
+  //Evaluate Result
+  let answer = countries[flagQuestions[flagQuestions.length - 1]];
+  if (checked == answer) {
+    trueScore++;
+    rightAns.push(checked);
+    correct.textContent = `True: ${trueScore}`;
+  } else {
+    wrongScore++;
+    wrongAns.push({actual: answer, checked});
+    wrong.textContent = `False: ${wrongScore}`;
+  }
+
+  //Create New Question
+  createGameArea();
 });
 
 flagsButton.addEventListener("click", () => {
+  createGameArea();
   startDiv.classList.add("d-none");
   flagsDiv.classList.remove("d-none");
   let minute = 0, second = 50;
